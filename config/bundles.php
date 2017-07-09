4451f51b4733febb618781e0e517d9f6cae33f21
yeclense <dani@yecladeyeltes.net>
2017-07-09 19:38:27 +0200

    demo completed

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,10 @@
 
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
+    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle' => ['all' => true],
 ];
---
+++ readme.md
@@ -0,0 +1,4 @@
+Symfony 4 demo using Symfony 3.3 setup with Symfony Flex.
+
+Instructions in this post:
+http://fabien.potencier.org/symfony4-demo.html
